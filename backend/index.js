8f6434a423c9baf2de702f196f3408ff04467cf8
bhavyae-pokhrel <bhavyaeprasadpokhrel@gmail.com>
2024-04-24 16:28:14 +0530

    food

---
+++ backend/index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const port =5000
+//start
+const cors=require('cors') 
+const stripe=require("stripe")("sk_test_51P7b5USFN6I3Hid9yWDOYTBqvb7aqLcal8VknOQyRiwjVII7wa7JCZYuZsdmj7eocmLx5huAWxvCcCXTGsz2xHRa00n4r6C4QM")
+// end
+const mongoDB =require("./db") 
+mongoDB();
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin","http://localhost:3500");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+})
+
+app.use(express.json ())
+// start
+app.use(cors());
+app.post("/api/create-checkout-session",async(req,res)=>{
+  const {products}=req.body;
+  console.log('index.js PRODUCTS->',products);
+
+  const lineItems =products.map((product)=>({
+       price_data:{ 
+        currency:'inr',
+        product_data:{
+          name :product.name
+        },
+        unit_amount:product.price * 100,  
+       },
+       quantity:product.qty 
+  }));
+  const session=await stripe.checkout.sessions.create({
+    payment_method_types:["card"], 
+    line_items:lineItems,
+    mode:"payment",
+    success_url:"http://localhost:3500/myorder",
+    cancel_url:"http://localhost:3500/cancel",
+  })
+  res.json({id:session.id})
+})
+// end
+app.use('/api', require("./Routes/CreatUser"));
+app.use('/api', require("./Routes/DisplayData"));
+app.use('/api', require("./Routes/OrderData"));
+
+app.get('/', (req, res ) => {
+  res.send('Hello World -->')
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
