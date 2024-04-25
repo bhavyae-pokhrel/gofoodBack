9add98b48756091b77b735cc4b4aea7385a6ff75
bhavyae-pokhrel <bhavyaeprasadpokhrel@gmail.com>
2024-04-25 10:14:59 +0530

    food

--- backend/index.js
+++ backend/index.js
@@ -41,8 +41,10 @@ app.post("/api/create-checkout-session",async(req,res)=>{
     payment_method_types:["card"], 
     line_items:lineItems,
     mode:"payment",
-    success_url:"http://localhost:3500/myorder",
-    cancel_url:"http://localhost:3500/cancel",
+    //success_url:"http://localhost:3500/myorder",
+    //cancel_url:"http://localhost:3500/cancel",
+    success_url:"https://gofoodfront-wiuy.onrender.com//myorder",
+    cancel_url:"https://gofoodfront-wiuy.onrender.com//cancel",
   })
   res.json({id:session.id})
 })
